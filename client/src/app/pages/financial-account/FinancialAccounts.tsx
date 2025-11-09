454e64388562ea3f04ca5c98235705738a0395c3
Jeanpiere Solorzano <jeanpksolo@gmail.com>
2025-11-09 17:15:47 -0500

    add financial account page

--- client/src/app/pages/financial-account/FinancialAccounts.tsx
+++ client/src/app/pages/financial-account/FinancialAccounts.tsx
@@ -1,53 +1,26 @@
-import { useEffect, useState } from 'react';
 import type { FinancialAccountObjectType } from '../../../objects/financial-account/financial-account.type';
-import Decimal from 'decimal.js';
 import { List } from 'antd';
 import { BankOutlined } from '@ant-design/icons';
 
 type Props = {
-  financialFinancialAccounts: FinancialAccountObjectType[];
+  financialAccounts: FinancialAccountObjectType[];
 };
 
-export const FinancialAccounts: React.FC<Props> = ({
-  financialFinancialAccounts,
-}) => {
-  const [totalValue, setTotalValue] = useState<number | null>(null);
-
-  const calculateTotalAccountValue = (
-    accounts: FinancialAccountObjectType[],
-  ): number => {
-    let accountsTotal: number = 0;
-    accounts.forEach((account) => {
-      if (account.balance) {
-        accountsTotal += Decimal(account.balance).toNumber();
-      }
-    });
-    return accountsTotal;
-  };
-
-  useEffect(() => {
-    setTotalValue(calculateTotalAccountValue(financialFinancialAccounts));
-  }, [totalValue]);
-
+export const FinancialAccounts: React.FC<Props> = ({ financialAccounts }) => {
   return (
-    <div>
-      <h3>Total Value: {totalValue ?? null}</h3>
-      <div>
-        <List
-          dataSource={financialFinancialAccounts}
-          renderItem={(account) => (
-            <List.Item>
-              <List.Item.Meta
-                avatar={<BankOutlined style={{ fontSize: '3em' }} />}
-                title={account.name}
-                description={account.type}
-              />
-              <div>{account.balance}</div>
-            </List.Item>
-          )}
-          style={{ border: 'solid', paddingLeft: '10px', paddingRight: '20px' }}
-        />
-      </div>
-    </div>
+    <List
+      dataSource={financialAccounts}
+      renderItem={(account) => (
+        <List.Item>
+          <List.Item.Meta
+            avatar={<BankOutlined style={{ fontSize: '3em' }} />}
+            title={account.name}
+            description={account.type}
+          />
+          <div>{account.balance}</div>
+        </List.Item>
+      )}
+      style={{ border: 'solid', paddingLeft: '10px', paddingRight: '20px' }}
+    />
   );
 };
