9d2423427c303f101a3321024f41ea4a9e5c517b
Jeanpiere Solorzano <jeanpksolo@gmail.com>
2025-11-11 20:08:50 -0500

    add queries and mutations

---
+++ client/src/mutations/UpdateTransaction.ts
@@ -0,0 +1,36 @@
+import { gql } from '@apollo/client';
+import type { TypedDocumentNode } from '@apollo/client';
+import type {
+  TransactionObjectType,
+  TransactionUpdateInput,
+  TransactionWhereUniqueInput,
+} from '../objects/transaction/transaction.type';
+
+type MutationReturnType = {
+  updateTransaction: TransactionObjectType;
+};
+
+type MutationVariables = {
+  data: TransactionUpdateInput;
+  where: TransactionWhereUniqueInput;
+};
+
+export const UPDATE_TRANSACTION: TypedDocumentNode<
+  MutationReturnType,
+  MutationVariables
+> = gql`
+  mutation UpdateTransaction(
+    $data: TransactionUpdateInput!
+    $where: TransactionWhereUniqueInput!
+  ) {
+    updateTransaction(data: $data, where: $where) {
+      id
+      amount
+      name
+      date
+      type
+      category
+      frequency
+    }
+  }
+`;
