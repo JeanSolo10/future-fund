9765c9a7520e17e5df2439902c392377273cd4ed
Jeanpiere Solorzano <jeanpksolo@gmail.com>
2026-01-25 07:49:34 -0500

    update text and style

--- client/src/features/transactions/TransactionsSummary.tsx
+++ client/src/features/transactions/TransactionsSummary.tsx
@@ -11,12 +11,16 @@ export const TransactionSummary: React.FC<Props> = ({
     <div className="transaction-summary">
       <div className="income-summary">
         <p>INCOME</p>
-        <p>{totalIncome ? `$${totalIncome}` : '$0.00'}</p>
+        <p className="expense-total-amount">
+          {totalIncome ? `$${totalIncome}` : '$0.00'}
+        </p>
       </div>
       <div className="summary-divider"></div>
       <div className="expense-summary">
-        <p>EXPENSE</p>
-        <p>{totalExpenses ? `$${totalExpenses}` : '$0.00'}</p>
+        <p>EXPENSES</p>
+        <p className="expense-total-amount">
+          {totalExpenses ? `$${totalExpenses}` : '$0.00'}
+        </p>
       </div>
     </div>
   );
