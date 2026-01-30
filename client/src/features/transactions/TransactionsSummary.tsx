bce86f01b7692aaff1ff7b7e516657adbbe62b7e
Jeanpiere Solorzano <jeanpksolo@gmail.com>
2026-01-30 07:37:28 -0500

    add remaining balance to summary

--- client/src/features/transactions/TransactionsSummary.tsx
+++ client/src/features/transactions/TransactionsSummary.tsx
@@ -1,3 +1,5 @@
+import Decimal from 'decimal.js';
+
 type Props = {
   totalIncome?: string;
   totalExpenses?: string;
@@ -7,20 +9,33 @@ export const TransactionSummary: React.FC<Props> = ({
   totalIncome,
   totalExpenses,
 }) => {
+  let income = totalIncome ?? '0';
+  let expenses = totalExpenses ?? '0';
+
+  const calculateRemainingBalance = (): string => {
+    const remaining = new Decimal(income).minus(new Decimal(expenses));
+    return `$${remaining.toDecimalPlaces(2).toString()}`;
+  };
+
   return (
     <div className="transaction-summary">
       <div className="income-summary">
         <p>INCOME</p>
         <p className="expense-total-amount">
-          {totalIncome ? `$${totalIncome}` : '$0.00'}
+          {totalIncome ? `$${income}` : '$0.00'}
         </p>
       </div>
-      <div className="summary-divider"></div>
+      <div className="summary-divider" />
       <div className="expense-summary">
         <p>EXPENSES</p>
         <p className="expense-total-amount">
-          {totalExpenses ? `$${totalExpenses}` : '$0.00'}
+          {totalExpenses ? `$${expenses}` : '$0.00'}
         </p>
+      </div>
+      <div className="summary-divider" />
+      <div className="expense-summary">
+        <p>REMAINING</p>
+        <p className="remaining-total-amount">{calculateRemainingBalance()}</p>
       </div>
     </div>
   );
