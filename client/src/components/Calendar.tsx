94a9542a69350e019d83723647d8adb6a75ed674
Jeanpiere Solorzano <jeanpksolo@gmail.com>
2026-03-08 15:13:12 -0400

    update calendar configuration

--- client/src/components/Calendar.tsx
+++ client/src/components/Calendar.tsx
@@ -2,7 +2,17 @@ import { Calendar } from 'antd';
 import type { DateTime } from 'luxon';
 import luxonGenerateConfig from 'rc-picker/lib/generate/luxon';
 
-const CustomAntdCalendar =
-  Calendar.generateCalendar<DateTime>(luxonGenerateConfig);
+const CustomAntdCalendar = Calendar.generateCalendar<DateTime>({
+  ...luxonGenerateConfig,
+  locale: {
+    ...luxonGenerateConfig.locale,
+    getWeekFirstDay: () => 0,
+  },
+});
+
+/* todo: create specific calendars based on user preference
+export const CalendarSunday = createCalendar(0);
+export const CalendarMonday = createCalendar(1);
+*/
 
 export default CustomAntdCalendar;
