e4c8e9d72ce840dcd0007c9a3581c3781cbd286d
Jeanpiere Solorzano <jeanpksolo@gmail.com>
2026-09-06 09:24:12 -0400

    remove unnecessary helper function

--- client/src/features/transactions/transactionHelper.ts
+++ client/src/features/transactions/transactionHelper.ts
@@ -1,30 +1,9 @@
 import { DateTime } from 'luxon';
-import type { TransactionCreateInput } from '../../object-types/transaction/transaction.type';
-import type { SemiMonthlyEnums } from './types';
 import {
   TransactionFrequencyEnum,
   type TransactionFrequency,
 } from '../../object-types/transaction/transaction.enums';
 import type { FormInstance } from 'antd';
-
-// todo: implement this correctly
-export const sanitizeCreateFormData = (
-  values: TransactionCreateInput & { semiMonthlyDates?: SemiMonthlyEnums },
-): TransactionCreateInput => {
-  const { semiMonthlyDates, ...restData } = values;
-
-  if (values.semiMonthlyDates) {
-    const isFirstAndFifthTeen = semiMonthlyDates?.FIFTEEN_AND_LAST_DAY;
-
-    return {
-      ...restData,
-    };
-  }
-
-  return {
-    ...restData,
-  };
-};
 
 // Limit selection to 1st and 15th for semi-monthly transactions
 export const disableDatesForSemiMonthlyTransaction = (date: DateTime) => {
